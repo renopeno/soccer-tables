694e1a42259ff83807fdcf49615d3a4c5183df07
Renato <renato.mandic@gmail.com>
2016-10-07 14:00:41 +0200

    styled bg, added table component

---
+++ src/TableBody.js
@@ -0,0 +1,15 @@
+// TableBody.js
+import React, { Component } from 'react';
+
+
+class TableBody extends Component {
+  render () {
+    return (
+      <div className="Table">
+
+      </div>
+    );
+  }
+}
+
+export default TableBody;
---
+++ src/TableHeader.js
@@ -0,0 +1,15 @@
+//TableHeader.js
+import React, { Component } from 'react';
+
+
+class TableHeader extends Component {
+  render () {
+    return (
+      <div className="Table--header">
+        This is table header
+      </div>
+    );
+  }
+}
+
+export default TableHeader;
